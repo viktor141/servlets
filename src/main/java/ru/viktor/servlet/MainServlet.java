b7a5325f5ce63c5e47b3ebc7e7bdacd656f7e8f8
viktor141 <31136336+viktor141@users.noreply.github.com>
2023-06-12 18:29:35 +0400

    Java Config

--- src/main/java/ru/viktor/servlet/MainServlet.java
+++ src/main/java/ru/viktor/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ru.viktor.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.viktor.config.JavaConfig;
 import ru.viktor.controller.PostController;
 import ru.viktor.repository.PostRepository;
 import ru.viktor.service.PostService;
@@ -20,9 +22,9 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+
+        controller = (PostController) context.getBean("postController");
     }
 
     @Override
